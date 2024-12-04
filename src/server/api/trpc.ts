fea92b1096332dbf1ec0bcc5f127be26717aa411
sukrit101 <sukrit@m0.ventures>
2024-12-04 21:53:05 +0530

    chore: setting up tailwind, removing prisma

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -6,11 +6,10 @@
  * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
  * need to use are documented accordingly near the end.
  */
-import { initTRPC } from "@trpc/server";
+import { currentUser } from "@clerk/nextjs/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
-
-import { db } from "~/server/db";
 
 /**
  * 1. CONTEXT
@@ -26,7 +25,6 @@ import { db } from "~/server/db";
  */
 export const createTRPCContext = async (opts: { headers: Headers }) => {
   return {
-    db,
     ...opts,
   };
 };
@@ -79,21 +77,20 @@ export const createTRPCRouter = t.router;
  * You can remove this if you don't like it, but it can help catch unwanted waterfalls by simulating
  * network latency that would occur in production but not in local development.
  */
-const timingMiddleware = t.middleware(async ({ next, path }) => {
-  const start = Date.now();
+const seshData = t.middleware(async ({ next }) => {
+  const user = await currentUser();
 
-  if (t._config.isDev) {
-    // artificial delay in dev
-    const waitMs = Math.floor(Math.random() * 400) + 100;
-    await new Promise((resolve) => setTimeout(resolve, waitMs));
+  if (!user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    });
   }
 
-  const result = await next();
-
-  const end = Date.now();
-  console.log(`[TRPC] ${path} took ${end - start}ms to execute`);
-
-  return result;
+  return next({
+    ctx: {
+      user,
+    },
+  });
 });
 
 /**
@@ -103,4 +100,5 @@ const timingMiddleware = t.middleware(async ({ next, path }) => {
  * guarantee that a user querying is authorized, but you can still access user session data if they
  * are logged in.
  */
-export const publicProcedure = t.procedure.use(timingMiddleware);
+export const publicProcedure = t.procedure;
+export const privateProcedure = t.procedure.use(seshData);
